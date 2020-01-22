1427f239044dc7e65aa9216b1620f8486ad76f74
Logan Negley <logannegley@gmail.com>
2020-01-21 18:48:24 -0600

    onChange added to input

--- team-builder/src/components/AddMember.js
+++ team-builder/src/components/AddMember.js
@@ -3,19 +3,27 @@ import React, {useState} from 'react';
 
 function AddMember() {
 
+const [input, setInput] = useState('')
+
+    const logInput= event => {
+        setInput(event.target.value);
+        // console.log(event.target.value)
+    }
+
   return (
     <div className="form-wrapper">
         <form>
             <label>
-                <input></input>
+                <input type='text' onChange={logInput}/>
             </label>
             <label>
-                <input></input>
+                <input type='text'/>
             </label>
             <label>
-                <input></input>
+                <input type='text'/>
             </label>
         </form>
+        <h1>{input}</h1>
     </div>
   );
 }
