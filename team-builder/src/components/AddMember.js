daa06d58f5666c33aa9d9b614721f68b8a8b6eb9
Logan Negley <logannegley@gmail.com>
2020-01-22 19:56:40 -0600

    added email to state

--- team-builder/src/components/AddMember.js
+++ team-builder/src/components/AddMember.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 function AddMember(props) {
 console.log( props)
-const [name, setName] = useState({name:''});
+const [name, setName] = useState({name:'', email:''});
 // const [email, setEmail] = useState('')
 // const [positon, setPosition] = useState('')
 
@@ -12,23 +12,25 @@ const [name, setName] = useState({name:''});
         setName( {name: event.target.value});
     };
 
-    const submit = () =>{
-        console.log(name);
+    const submit = (event) =>{
+        event.preventDefault();
+        props.addNewMember(name);
+        setName({name: ''});
     };
 
   return (
     <div className="form-wrapper">
-        <form>
+        <form  onSubmit= {submit} >
             <label htmlFor='name'>
                 <input type='text' id='name' placeholder='name' onChange={handleChanges}/>
             </label>
-            {/* <label htmlFor='email'>
-                <input type='text' id='email' placeholder='email' onChange={logInput}/>
-            </label>
-            <label htmlFor='position'>
+             <label htmlFor='email'>
+                <input type='text' id='email' placeholder='email' onChange={handleChanges} />
+            </label> 
+            {/* <label htmlFor='position'>
                 <input type='text' id='position' placeholder='positon'/>
-            </label> */}
-            <button type='submit' onSubmit= {() => submit()} >Submit</button>
+            </label>  */}
+            <button type='submit'>Submit</button>
         </form>
     </div>
   );
